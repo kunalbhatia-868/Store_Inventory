dff8244f26e4a549ef91c094167457e7ea996842
kunalbhatia-868 <kunalbhatia868@gmail.com>
2023-04-01 17:59:41 +0530

    store updated

--- backend/urls.py
+++ backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('accounts/',include('user.urls')),
     path('admin/', admin.site.urls),
+    path('store/',include('store.urls')),
 ]
